56591292fb50f0a0b618dc07b319c12c0a9bd1a8
Maciej Brencz <maciej.brencz@gmail.com>
2017-10-21 16:53:26 +0200

    Use underscores in table names

--- sql/0004-redundant-indices.sql
+++ sql/0004-redundant-indices.sql
@@ -1,16 +1,16 @@
 -- Detect redundant indices
 --
 -- https://github.com/macbre/index-digest/issues/4
-DROP TABLE IF EXISTS `0004-id-foo`;
-CREATE TABLE `0004-id-foo` (
+DROP TABLE IF EXISTS `0004_id_foo`;
+CREATE TABLE `0004_id_foo` (
 	`id` int(9) NOT NULL AUTO_INCREMENT,
 	`foo` varbinary(16) NOT NULL DEFAULT '',
 	PRIMARY KEY (`id`,`foo`),
 	UNIQUE KEY `idx` (`id`,`foo`)
 );
 
-DROP TABLE IF EXISTS `0004-id-foo-bar`;
-CREATE TABLE `0004-id-foo-bar` (
+DROP TABLE IF EXISTS `0004_id_foo_bar`;
+CREATE TABLE `0004_id_foo_bar` (
 	`id` int(9) NOT NULL AUTO_INCREMENT,
 	`foo` varbinary(16) NOT NULL DEFAULT '',
 	`bar` varbinary(16) NOT NULL DEFAULT '',
@@ -19,4 +19,3 @@ CREATE TABLE `0004-id-foo-bar` (
 	KEY `idx_foo_bar` (`foo`, `bar`),
 	KEY `idx_id_foo` (`id`, `foo`)
 );
-
