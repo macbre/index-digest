adef248a3c3f56ed2842ec517aa807bf719c5b35
Maciej Brencz <maciej.brencz@gmail.com>
2017-10-22 20:18:02 +0200

    Implement check_not_used_tables linter

--- indexdigest/linters/not_used_columns_and_tables.py
+++ indexdigest/linters/not_used_columns_and_tables.py
@@ -1,48 +1,41 @@
 """
 This linter checks for not used columns and tables by going through SELECT queries
 """
-from collections import defaultdict
-
 from indexdigest.utils import LinterEntry, is_select_query
 
 
-def check_not_used_columns_and_tables(database, queries):
+def check_not_used_tables(database, queries):
     """
     :type database  indexdigest.database.Database
     :type queries list[str]
     :rtype: list[LinterEntry]
     """
-    reports = []
 
     # get database meta-data
     tables = database.get_tables()
-    columns = {}
-
-    for table in database.get_tables():
-        columns[table] = database.get_table_metadata(table)['columns']
 
     # analyze only SELECT queries from the log
     queries = filter(is_select_query, queries)
 
-    used_columns = defaultdict(list)
     used_tables = set()
 
     for query in queries:
-        res = database.explain_query(query)
-        print(query, columns, list(res))
+        # run EXPLAIN for each query from the log
+        for row in database.explain_query(query):
+            if 'table' in row:
+                used_tables.add(row['table'])
 
-    """
-        indices = database.get_table_indices(table)
-        for (redundant_index, suggested_index) in get_redundant_indices(indices):
-            reports.append(
-                LinterEntry(linter_type='redundant_indices', table_name=table,
-                            message='{} index can be removed as redundant (covered by {})'.
-                            format(redundant_index, suggested_index),
-                            context={'redundant': redundant_index, 'covered_by': suggested_index}))
-    """
+    # now check which tables were not used
+    not_used_tables = filter(
+        lambda t: t not in used_tables,
+        tables
+    )
 
-    print(list(queries))
-    print(list(tables))
-    print(columns)
+    # generate reports
+    reports = []
+    for table in not_used_tables:
+        reports.append(
+            LinterEntry(linter_type='not_used_tables', table_name=table,
+                        message='Table was not used by provided queries'))
 
     return reports
--- indexdigest/test/linters/test_not_used_columns_and_tables.py
+++ indexdigest/test/linters/test_not_used_columns_and_tables.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 from unittest import TestCase
 
-from indexdigest.linters.not_used_columns_and_tables import check_not_used_columns_and_tables
+from indexdigest.linters.not_used_columns_and_tables import check_not_used_tables
 from indexdigest.database import Database
 from indexdigest.test import DatabaseTestMixin
 
@@ -15,16 +15,22 @@ class LimitedViewDatabase(Database, DatabaseTestMixin):
         return ['0006_not_used_columns', '0006_not_used_tables']
 
 
-class TestNotUsedColumnsAndTables(TestCase):
+class TestNotUsedTables(TestCase):
 
     @property
     def connection(self):
         return LimitedViewDatabase.connect_dsn(DatabaseTestMixin.DSN)
 
-    def test_not_used_columns_and_tables(self):
+    def test_not_used_tables(self):
         with open('sql/0006-not-used-columns-and-tables-log') as fp:
             queries = fp.readlines()
 
-        results = check_not_used_columns_and_tables(database=self.connection, queries=queries)
+        reports = check_not_used_tables(database=self.connection, queries=queries)
+
+        print(reports)
+
+        self.assertEqual(len(reports), 1)
+        self.assertEqual(str(reports[0]), '0006_not_used_tables: Table was not used by provided queries')
+        self.assertEqual(reports[0].table_name, '0006_not_used_tables')
 
         # assert False
