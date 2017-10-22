e2180151bc1edd9c470cd6461b2ead301c472b8b
Maciej Brencz <maciej.brencz@gmail.com>
2017-10-22 20:06:49 +0200

    Introduce indexdigest.query helpers using sqlparse lib

---
+++ indexdigest/query.py
@@ -0,0 +1,47 @@
+"""
+This module provides SQL query parsing functions
+"""
+import sqlparse
+
+from sqlparse.sql import Token, TokenList
+from sqlparse.tokens import Name, Whitespace
+
+
+def get_query_tokens(query):
+    """
+    :type query str
+    :rtype: list[Token]
+    """
+    tokens = TokenList(sqlparse.parse(query)[0].tokens).flatten()
+
+    return filter(
+        lambda token: token.ttype is not Whitespace,
+        tokens
+    )
+
+
+def get_query_columns(query):
+    """
+    :type query str
+    :rtype: list[str]
+    """
+    columns = []
+    last_keyword = None
+    last_token = None
+
+    for token in get_query_tokens(query):
+        if token.is_keyword and token.value.upper() not in ['AS', 'AND', 'OR']:
+            # keep the name of the last keyword, e.g. SELECT, FROM, WHERE, (ORDER) BY
+            last_keyword = token.value.upper()
+            # print('keyword', last_keyword)
+        elif token.ttype is Name:
+            # analyze the name tokens, column names and where condition values
+            if last_keyword in ['SELECT', 'WHERE', 'BY'] and last_token not in ['AS']:
+                # print(last_keyword, last_token, token.value)
+
+                if token.value not in columns:
+                    columns.append(token.value)
+
+        last_token = token.value.upper()
+
+    return columns
---
+++ indexdigest/test/core/test_query.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from indexdigest.query import get_query_columns
+
+
+class TestUtils(TestCase):
+
+    def test_get_query_columns(self):
+        self.assertListEqual([],
+                             get_query_columns('SELECT * FROM `test_table`'))
+
+        self.assertListEqual(['foo'],
+                             get_query_columns('SELECT foo FROM `test_table`'))
+
+        self.assertListEqual(['id', 'foo'],
+                             get_query_columns('SELECT id, foo FROM test_table WHERE id = 3'))
+
+        self.assertListEqual(['foo', 'count', 'id'],
+                             get_query_columns('SELECT foo, count(*) as bar FROM `test_table` WHERE id = 3'))
+
+        self.assertListEqual(['foo', 'test'],
+                             get_query_columns('SELECT foo, test as bar FROM `test_table`'))
+
+        # assert False
