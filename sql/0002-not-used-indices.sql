900da8e091abdac0553d37c673fecf883b05fc32
Maciej Brencz <maciej.brencz@gmail.com>
2017-10-23 19:28:19 +0200

    not_used_indices: implement a linter (see #2)

--- sql/0002-not-used-indices.sql
+++ sql/0002-not-used-indices.sql
@@ -6,12 +6,13 @@ CREATE TABLE `0002_not_used_indices` (
 	`id` int(9) NOT NULL AUTO_INCREMENT,
 	`foo` varchar(16) NOT NULL DEFAULT '',
 	`test` varchar(16) NOT NULL DEFAULT '',
+	`bar` varchar(16),
 	PRIMARY KEY (`id`),
 	KEY `test_id_idx` (`test`, `id`),
 	KEY `foo_id_idx` (`foo`, `id`)
 );
 
 INSERT INTO 0002_not_used_indices VALUES
-    (NULL, 'test', ''),
-    (NULL, 'foo', 'test'),
-    (NULL, 'foo', '');
+    (NULL, 'test', '', NULL),
+    (NULL, 'foo', 'test', NULL),
+    (NULL, 'foo', '', NULL);
