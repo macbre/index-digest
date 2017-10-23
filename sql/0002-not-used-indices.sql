c9a14f0f8a246eb397b39b6a4db5d13225886b8f
Maciej Brencz <maciej.brencz@gmail.com>
2017-10-23 18:43:55 +0200

    Report not used indices - add test cases

---
+++ sql/0002-not-used-indices.sql
@@ -0,0 +1,17 @@
+-- Report not used indices
+--
+-- https://github.com/macbre/index-digest/issues/2
+DROP TABLE IF EXISTS `0002_not_used_indices`;
+CREATE TABLE `0002_not_used_indices` (
+	`id` int(9) NOT NULL AUTO_INCREMENT,
+	`foo` varchar(16) NOT NULL DEFAULT '',
+	`test` varchar(16) NOT NULL DEFAULT '',
+	PRIMARY KEY (`id`),
+	KEY `test_id_idx` (`test`, `id`),
+	KEY `foo_id_idx` (`foo`, `id`)
+);
+
+INSERT INTO 0002_not_used_indices VALUES
+    (NULL, 'test', ''),
+    (NULL, 'foo', 'test'),
+    (NULL, 'foo', '');
