971ba3a4a1c562676e39ea45744cfb1d08291470
Maciej Brencz <maciej.brencz@gmail.com>
2017-10-24 20:36:27 +0200

    Introduce DatabaseWithMockedRow helper for tests

--- indexdigest/test/__init__.py
+++ indexdigest/test/__init__.py
@@ -7,3 +7,22 @@ class DatabaseTestMixin(object):
     @property
     def connection(self):
         return Database.connect_dsn(self.DSN)
+
+
+class DatabaseWithMockedRow(Database):
+
+    def __init__(self, mocked_row):
+        super(DatabaseWithMockedRow, self).__init__(db='', host='', passwd='', user='')
+        self.row = mocked_row
+
+    @property
+    def connection(self):
+        raise Exception('Class {} needs to mock the query_* method'.format(self.__class__.__name__))
+
+    def query(self, sql, cursor=None):
+        self.query_logger.info(sql)
+        return [self.row]
+
+    def query_row(self, sql):
+        self.query_logger.info(sql)
+        return self.row
