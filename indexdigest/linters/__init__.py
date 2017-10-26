eeca7e49919ebba49b2b667352387f68cf7bf6d2
macbre <maciej.brencz@gmail.com>
2017-10-26 21:06:01 +0200

    Implement queries_not_using_index check (resolves #19)

--- indexdigest/linters/__init__.py
+++ indexdigest/linters/__init__.py
@@ -3,5 +3,5 @@ Contains linters used to check the database for improvements.
 """
 # expose linters
 from .not_used_columns_and_tables import check_not_used_tables, check_not_used_columns
-from .not_used_indices import check_not_used_indices
+from .not_used_indices import check_not_used_indices, check_queries_not_using_indices
 from .redundant_indices import check_redundant_indices
--- indexdigest/linters/not_used_indices.py
+++ indexdigest/linters/not_used_indices.py
@@ -3,7 +3,7 @@ This linter checks for not used indices by going through SELECT queries
 """
 import logging
 
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 from indexdigest.utils import LinterEntry, is_select_query
 
@@ -21,9 +21,8 @@ def explain_queries(database, queries):
         for row in database.explain_query(query):
             table_used = row['table']
             index_used = row['key']
-            extra = row['Extra']
 
-            yield (query, table_used, index_used, extra)
+            yield (query, table_used, index_used, row)
 
 
 def check_not_used_indices(database, queries):
@@ -56,5 +55,33 @@ def check_not_used_indices(database, queries):
                                 message='"{}" index was not used by provided queries'.
                                 format(index.name),
                                 context={"not_used_index": index}))
+
+    return reports
+
+
+def check_queries_not_using_indices(database, queries):
+    """
+    :type database  indexdigest.database.Database
+    :type queries list[str]
+    :rtype: list[LinterEntry]
+    """
+    reports = []
+
+    for (query, table_used, index_used, explain_row) in explain_queries(database, queries):
+        if index_used is None:
+            context = OrderedDict()
+            context['query'] = query
+
+            # https://dev.mysql.com/doc/refman/5.7/en/explain-output.html#explain-extra-information
+            context['explain_extra'] = explain_row['Extra']
+            context['explain_rows'] = explain_row['rows']
+            context['explain_filtered'] = explain_row.get('filtered')  # can be not set
+            context['explain_possible_keys'] = explain_row['possible_keys']
+
+            reports.append(
+                LinterEntry(linter_type='queries_not_using_index', table_name=table_used,
+                            message='"{}" query did not make use of any index'.
+                            format('{}...'.format(query[:50]) if len(query) > 50 else query),
+                            context=context))
 
     return reports
