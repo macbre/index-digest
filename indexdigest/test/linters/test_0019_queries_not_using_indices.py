1d30503acb2885d4cd92f1445ac32cd810a9ae83
macbre <maciej.brencz@gmail.com>
2017-11-09 20:24:20 +0100

    queries_not_using_index: separate tests

--- indexdigest/test/linters/test_0019_queries_not_using_indices.py
+++ indexdigest/test/linters/test_0019_queries_not_using_indices.py
@@ -2,25 +2,8 @@ from __future__ import print_function
 
 from unittest import TestCase
 
-from indexdigest.linters.linter_0002_not_used_indices import check_not_used_indices, \
-    check_queries_not_using_indices
+from indexdigest.linters.linter_0019_queries_not_using_indices import check_queries_not_using_indices
 from indexdigest.test import DatabaseTestMixin, read_queries_from_log
-
-
-class TestNotUsedIndices(TestCase, DatabaseTestMixin):
-
-    def test_not_used_indices(self):
-        reports = list(check_not_used_indices(
-            database=self.connection, queries=read_queries_from_log('0002-not-used-indices-log')))
-
-        print(reports)
-
-        self.assertEqual(len(reports), 1)
-        self.assertEqual(str(reports[0]), '0002_not_used_indices: "test_id_idx" index was not used by provided queries')
-        self.assertEqual(reports[0].table_name, '0002_not_used_indices')
-        self.assertEqual(str(reports[0].context['not_used_index']), 'KEY test_id_idx (test, id)')
-
-        # assert False
 
 
 class TestQueriesNotUsingIndices(TestCase, DatabaseTestMixin):
