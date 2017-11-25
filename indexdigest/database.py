ce01fdabf92cc633fea9c189159138568b6ec4f2
macbre <maciej.brencz@gmail.com>
2017-11-25 14:41:28 +0100

    Database: get_table_columns - maintain columns order

--- indexdigest/database.py
+++ indexdigest/database.py
@@ -253,16 +253,28 @@ class Database(DatabaseBase):
         :type table_name str
         :rtype: list[Column]
         """
+        # @see https://dev.mysql.com/doc/refman/5.7/en/show-columns.html
+        columns = [
+            row['Field']
+            for row in self.query_dict_rows("SHOW COLUMNS FROM {}".format(table_name))
+        ]
+
         # @see https://dev.mysql.com/doc/refman/5.7/en/columns-table.html
         rows = self.query_dict_rows(
             "SELECT COLUMN_NAME as NAME, COLUMN_TYPE as TYPE, CHARACTER_SET_NAME, COLLATION_NAME "
-            "FROM information_schema.COLUMNS " + self._get_information_schema_where(table_name) +
-            " ORDER BY COLUMN_NAME")
+            "FROM information_schema.COLUMNS " + self._get_information_schema_where(table_name))
 
+        meta = dict()
+
+        for row in rows:
+            meta[row['NAME']] = Column(name=row['NAME'], column_type=row['TYPE'],
+                                       character_set=row['CHARACTER_SET_NAME'],
+                                       collation=row['COLLATION_NAME'])
+
+        # keep the order taken from SHOW COLUMNS
         return [
-            Column(name=row['NAME'], column_type=row['TYPE'],
-                   character_set=row['CHARACTER_SET_NAME'], collation=row['COLLATION_NAME'])
-            for row in rows
+            meta[column]
+            for column in columns
         ]
 
     def get_table_indices(self, table_name):
