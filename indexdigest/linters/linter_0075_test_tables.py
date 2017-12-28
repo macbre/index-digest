49f7550306a8bceb2f4fba22e7530ee8e233470a
macbre <maciej.brencz@gmail.com>
2017-12-28 21:59:34 +0100

    test_tables: introduce a linter (resolves #75)

---
+++ indexdigest/linters/linter_0075_test_tables.py
@@ -0,0 +1,31 @@
+"""
+This linter reports tables with "test" word in their name
+"""
+import re
+
+from indexdigest.utils import LinterEntry
+
+
+def is_test_table(table_name):
+    """
+    :type table_name str
+    :rtype: bool
+    """
+    return re.search(r'(^|_)test(_|$)', table_name) is not None
+
+
+def check_test_tables(database):
+    """
+    :type database  indexdigest.database.Database
+    :rtype: list[LinterEntry]
+    """
+    test_tables = [
+        table for table in database.get_tables()
+        if is_test_table(table)
+    ]
+
+    for table in test_tables:
+        yield LinterEntry(linter_type='test_tables', table_name=table,
+                          message='"{}" seems to be a test table'.
+                          format(table),
+                          context={'schema': database.get_table_schema(table)})
