895bfd0a904e71180e309c479face76b675f6523
macbre <maciej.brencz@gmail.com>
2017-12-29 19:31:46 +0100

    TestInsertIgnore: update the test

--- indexdigest/test/linters/test_0070_insert_ignore.py
+++ indexdigest/test/linters/test_0070_insert_ignore.py
@@ -22,6 +22,7 @@ class TestInsertIgnore(TestCase, DatabaseTestMixin):
 
     def test_is_insert_ignore_query(self):
         assert is_insert_ignore_query("INSERT IGNORE INTO 0070_insert_ignore VALUES ('2017-01-01', 9, 123);") is True
+        assert is_insert_ignore_query("INSERT IGN/*bar*/ORE INTO 0070_insert_ignore VALUES ('2017-01-01', 9, 123);") is True
         assert is_insert_ignore_query("Insert /* foo */ Ignore INTO 0070_insert_ignore VALUES ('2017-01-01', 9, 123);") is True
         assert is_insert_ignore_query("/* foo */ INSERT IGNORE INTO `0070_insert_ignore` VALUES (9, '123', '2017-01-01');") is True
 
@@ -34,13 +35,14 @@ class TestInsertIgnore(TestCase, DatabaseTestMixin):
 
         print(reports)
 
-        self.assertEqual(len(reports), 3)
+        self.assertEqual(len(reports), 4)
 
         self.assertEqual(str(reports[0]), '0070_insert_ignore: "INSERT IGNORE INTO `0070_insert_ignore` VALUES (9,..." query uses a risky INSERT IGNORE')
         self.assertEqual(reports[0].table_name, '0070_insert_ignore')
         self.assertEqual(str(reports[0].context['query']), "INSERT IGNORE INTO `0070_insert_ignore` VALUES (9, '123', '2017-01-01');")
         assert 'CREATE TABLE `0070_insert_ignore` (' in str(reports[0].context['schema'])
 
-        self.assertEqual(str(reports[1].context['query']), "INSERT  IGNORE INTO `0070_insert_ignore` VALUES ('123', 9, '2017-01-01');")
-        self.assertEqual(str(reports[2].context['query']), "INSERT /* foo */ IGNORE INTO `0070_insert_ignore` VALUES ('2017-01-01', 9, 123);")
+        self.assertEqual(str(reports[1].context['query']), "/* foo */ INSERT IGNORE INTO `0070_insert_ignore` VALUES (9, '123', '2017-01-01');")
+        self.assertEqual(str(reports[2].context['query']), "INSERT  IGNORE INTO `0070_insert_ignore` VALUES ('123', 9, '2017-01-01');")
+        self.assertEqual(str(reports[3].context['query']), "INSERT /* foo */ IGNORE INTO `0070_insert_ignore` VALUES ('2017-01-01', 9, 123);")
         # assert False
