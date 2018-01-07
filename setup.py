c8275dd9056d55893f11c17c8e0cc3f737bdb73a
macbre <maciej.brencz@gmail.com>
2018-01-07 15:15:18 +0100

    setup.py: add classifiers

--- setup.py
+++ setup.py
@@ -11,6 +11,31 @@ setup(
     license='MIT',
     description='Analyses your database queries and schema and suggests indices improvements',
     url='https://github.com/macbre/index-digest',
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: Database',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
     packages=find_packages(),
     install_requires=[
         'docopt==0.6.2',
