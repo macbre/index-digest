201ed70801553e1c5469425310072c44a671047b
macbre <maciej.brencz@gmail.com>
2018-01-13 13:03:28 +0100

    test_explain_queries: improve a test

--- indexdigest/test/test_0089_handle_sql_errors.py
+++ indexdigest/test/test_0089_handle_sql_errors.py
@@ -19,17 +19,18 @@ class ErrorsHandlingTest(TestCase, DatabaseTestMixin):
             database=self.connection,
             queries=self.queries)
 
-        self.assertListEqual(['t'], tables)
+        print(tables)
+
+        assert '0020_big_table' in tables
         # assert False
 
     def test_explain_queries(self):
         res = list(explain_queries(self.connection, self.queries))
+        tables_used = [item[1] for item in res]
 
-        print(res)
+        print(res, tables_used)
 
-        (_, table_used, _, _) = res[0]
-        self.assertEquals(table_used, 't')
-
+        assert '0020_big_table' in tables_used
         # assert False
 
     def test_get_table_columns(self):
