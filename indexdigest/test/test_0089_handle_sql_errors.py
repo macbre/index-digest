fd70bc3e4f57cb08d0a1e8f7594881c7336d02e0
macbre <maciej.brencz@gmail.com>
2018-01-13 12:00:42 +0100

    explain_queries: add errors handling

--- indexdigest/test/test_0089_handle_sql_errors.py
+++ indexdigest/test/test_0089_handle_sql_errors.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 from unittest import TestCase
 
 from indexdigest.linters.linter_0006_not_used_columns_and_tables import get_used_tables_from_queries
+from indexdigest.utils import explain_queries
+
 from indexdigest.test import DatabaseTestMixin, read_queries_from_log
 
 
@@ -18,4 +20,15 @@ class ErrorsHandlingTest(TestCase, DatabaseTestMixin):
             queries=self.queries)
 
         self.assertListEqual(['t'], tables)
+        # assert False
+
+    def test_explain_queries(self):
+        res = list(explain_queries(self.connection, self.queries))
+
+        print(res)
+
+        (_, table_used, _, _) = res[0]
+        self.assertEquals(len(res), 1)
+        self.assertEquals(table_used, 't')
+
         # assert False
