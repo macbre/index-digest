6a97fc9ece535bd2d5223249ed3549789c61fc13
macbre <maciej.brencz@gmail.com>
2018-01-20 15:56:00 +0100

    TestSchemaWithPartition: assert that the comment is gone

--- indexdigest/test/test_0107_schema_partitions.py
+++ indexdigest/test/test_0107_schema_partitions.py
@@ -11,12 +11,4 @@ class TestSchemaWithPartition(TestCase, DatabaseTestMixin):
         schema = self.connection.get_table_schema('0107_schema_partitions')
         print(schema)
 
-        assert schema == """
-CREATE TABLE `0107_schema_partitions` (
-  `firstname` varchar(25) NOT NULL,
-  `lastname` varchar(25) NOT NULL,
-  `username` varchar(16) NOT NULL,
-  `email` varchar(35) DEFAULT NULL,
-  `joined` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-        """.strip()
+        assert '/*!50100' not in schema
