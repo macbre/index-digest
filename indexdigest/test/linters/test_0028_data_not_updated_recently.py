f12c511068b99ae27634f3681757bce39fd871f7
macbre <maciej.brencz@gmail.com>
2018-02-01 22:00:24 +0100

    test_0028_data_not_updated_recently: more assertioms

--- indexdigest/test/linters/test_0028_data_not_updated_recently.py
+++ indexdigest/test/linters/test_0028_data_not_updated_recently.py
@@ -20,6 +20,15 @@ class TestLinter(TestCase, DatabaseTestMixin):
 
         assert len(reports) == 1
 
+        assert str(reports[0]).startswith('0028_data_not_updated_recently: "0028_data_not_updated_recently" '
+                                          'has the latest row added 4')  # 40 days ago
+        assert str(reports[0]).endswith('consider checking if it should be up-to-date')
+        self.assertAlmostEquals(reports[0].context['diff_days'], 40)
+        assert reports[0].table_name == '0028_data_not_updated_recently'
+
+        assert 'data_since' in reports[0].context
+        assert 'data_until' in reports[0].context
+
     def test_data_not_updated_recently_with_custom_threshold(self):
         env = {
             'INDEX_DIGEST_DATA_NOT_UPDATED_RECENTLY_THRESHOLD_DAYS': str(60 * 86400)
