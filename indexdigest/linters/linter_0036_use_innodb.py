f008e4e72120fde3e48436e420f0f62acc051452
macbre <maciej.brencz@gmail.com>
2018-03-17 16:56:40 +0100

    use_innodb | in MySQL 8.0 information_schema tables columns are uppercase

--- indexdigest/linters/linter_0036_use_innodb.py
+++ indexdigest/linters/linter_0036_use_innodb.py
@@ -11,16 +11,17 @@ def check_use_innodb(database):
     :type database  indexdigest.database.Database
     :rtype: list[LinterEntry]
     """
-    res = database.query_dict_rows("SELECT table_name, engine FROM information_schema.tables "
-                                   "WHERE engine <> 'InnoDB' and table_schema = '{}'".
+    # in MySQL 8.0 information_schema tables columns are uppercase
+    res = database.query_dict_rows("SELECT TABLE_NAME, ENGINE FROM information_schema.tables "
+                                   "WHERE ENGINE <> 'InnoDB' and TABLE_SCHEMA = '{}'".
                                    format(database.db_name))
 
     for row in res:
         context = OrderedDict()
-        context['schema'] = database.get_table_schema(row['table_name'])
-        context['engine'] = row['engine']
+        context['schema'] = database.get_table_schema(row['TABLE_NAME'])
+        context['engine'] = row['ENGINE']
 
-        yield LinterEntry(linter_type='use_innodb', table_name=row['table_name'],
-                          message='"{table_name}" uses {engine} storage engine'.
+        yield LinterEntry(linter_type='use_innodb', table_name=row['TABLE_NAME'],
+                          message='"{TABLE_NAME}" uses {ENGINE} storage engine'.
                           format(**row),
                           context=context)
