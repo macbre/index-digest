480e6f1027b555c72f778c4bbce5434ed0db8f25
macbre <maciej.brencz@gmail.com>
2018-03-20 18:11:54 +0100

    non_utf_columns | handle blobs and other UTF character sets
    Resolves #143

--- indexdigest/linters/linter_0032_utf_latin_columns.py
+++ indexdigest/linters/linter_0032_utf_latin_columns.py
@@ -6,6 +6,27 @@ from collections import OrderedDict
 from indexdigest.utils import LinterEntry
 
 
+def is_text_column_latin(column):
+    """
+    :type column indexdigest.schema.Column
+    :rtype: bool
+    """
+    if not column.is_text_type():
+        return False
+
+    # ignore blob columns without specified character set
+    if column.character_set is None:
+        return False
+
+    # ignore utf8 columns
+    # utf8, ucs2, utf8mb4, utf16, utf16le, utf32
+    # @see https://dev.mysql.com/doc/refman/5.7/en/charset-unicode.html
+    if column.character_set.startswith('utf') or column.character_set in ['ucs2', 'binary']:
+        return False
+
+    return True
+
+
 def check_latin_columns(database):
     """
     :type database  indexdigest.database.Database
@@ -13,11 +34,7 @@ def check_latin_columns(database):
     """
     for table in database.get_tables():
         for column in database.get_table_columns(table):
-            if not column.is_text_type():
-                continue
-
-            # ignore utf8 columns
-            if column.character_set in ['utf8']:
+            if not is_text_column_latin(column):
                 continue
 
             # print([table, column, column.character_set, column.collation])
