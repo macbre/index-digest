4182cc325663b66635200b8c170db1033f621810
macbre <maciej.brencz@gmail.com>
2018-09-17 22:19:36 +0200

    Downgrade to pylint 1.9.3 - we do support Python 2.7

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=[
         'docopt==0.6.2',
         'coverage==4.5.1',
-        'pylint==2.1.1',
+        'pylint==1.9.3',
         'pytest==3.8.0',
         'PyYAML==3.13',
         'mysqlclient==1.3.13',
